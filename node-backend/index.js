6a73373299659192e735770b9691f4e7dc235b7e
Richard Varadi <ricsi19981007@gmail.com>
2020-11-28 19:02:20 +0100

    Fix index.js and Docker

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 var monngoURL = 'mongodb://mongo-service/database'
 
 mongoose.connect(monngoURL, { useNewUrlParser: true })
